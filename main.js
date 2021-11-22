a1ed889ae4f3f242e11e39e62b08c0df50a3dd2b
aquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-22 19:16:01 +0100

    add bonus: colore x, new item menu hamburger

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
-//Selezione hamburger menu e creazione variabile
 
+
+//Selezione hamburger menu e creazione variabile
 const hamb_menu = document.querySelector('.hamburger-menu');
 
 // selezione icona hamburger e creazione variabile
@@ -9,10 +10,16 @@ const icon_hamburger = document.querySelector('.fas.fa-bars');
 // selezione icona close e creazione variabile
 const icon_x = document.querySelector('.fas.fa-times');
 
-// creazione evento "click" menu show
+//creazione elemento lista hamburger menu
+const hamb_lista = document.querySelector('.hamburger-menu ul');
+hamb_lista.innerHTML += `<li><a href="#">Scopri il Salento</a></li>`;
+
+
+// creazione evento "click" menu show, cambio colore icona X
 icon_hamburger.addEventListener('click' , 
     function () {
         hamb_menu.style.display = "flex";
+        icon_x.style.color ="yellow";
     }
     
 );
@@ -24,3 +31,4 @@ icon_x.addEventListener('click' ,
     }
     
 );
+
