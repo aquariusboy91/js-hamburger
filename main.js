84085ff3b67ffe7ec229f987de76aa07350e9491
Acquariusboy91 <luca.pellegrino91@yahoo.it>
2021-11-22 15:17:41 +0100

    add-close

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const hamb_menu = document.querySelector('.hamburger-menu');
 const icon_hamburger = document.querySelector('.fas.fa-bars');
+const icon_x = document.querySelector('.fas.fa-times');
 console.log (hamb_menu);
 
 icon_hamburger.addEventListener('click' , 
     function () {
         hamb_menu.style.display = "flex";
+    }
+    
+);
+
+icon_x.addEventListener('click' , 
+    function () {
+        hamb_menu.style.display = "none";
     }
     
 );
